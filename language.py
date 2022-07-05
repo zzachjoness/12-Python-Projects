c4fcbeb4a0aad46481b05b674560a9bd9130e329
zzachjoness <zzachjoness@gmail.com>
2022-07-05 09:10:48 -0400

    add language file

---
+++ language.py
@@ -0,0 +1,38 @@
+
+dict_one = [
+  {"brand": "ford", 
+  "model": "mustang"
+  },
+  {"brand": "chevy",
+  "model": "corvette"
+  }]
+print(dict_one[0])
+print(dict_one[0]["brand"])
+print(dict_one[1])
+print(dict_one[1]["brand"])
+
+"""
+python list
+  used to store multiple items in a single variable
+  list items are ordered, changeable, and allow duplicate values
+  list items are indexed, the first item has index [0] .... [n]
+"""
+mylist = ["apple", "banana", "cherry"]
+print(mylist, ", ", mylist[0])
+
+
+"""
+python tuple
+  tuples are used to store multiple items in a single variable
+  tuples are ordered and unchangeable
+"""
+
+mytuple = ("apple", "banana", "cherry")
+print(mytuple)
+
+
+"""
+
+python 
+
+"""
--- madlibs.py
+++ madlibs.py
@@ -34,34 +34,3 @@ One = Madlibs("verb", "paragraph")
 print(One.inputs)
 print(One.madlib)
 One.showInputs("a")
-
-dict_one = [
-  {"brand": "ford", 
-  "model": "mustang"
-  },
-  {"brand": "chevy",
-  "model": "corvette"
-  }]
-print(dict_one[0])
-print(dict_one[0]["brand"])
-print(dict_one[1])
-print(dict_one[1]["brand"])
-
-"""
-python list
-  used to store multiple items in a single variable
-  list items are ordered, changeable, and allow duplicate values
-  list items are indexed, the first item has index [0] .... [n]
-"""
-mylist = ["apple", "banana", "cherry"]
-print(mylist, ", ", mylist[0])
-
-
-"""
-python tuple
-  tuples are used to store multiple items in a single variable
-  tuples are ordered and unchangeable
-"""
-
-mytuple = ("apple", "banana", "cherry")
-print(mytuple)
