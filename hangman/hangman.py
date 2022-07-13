ceb7af6377987f645d74de23c421ac6fc90b3ef9
zzachjoness <zzachjoness@gmail.com>
2022-07-13 15:41:13 -0400

    hangman chracter udpated

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -3,4 +3,5 @@ import random
 words = words.words
 random_word = random.choice(words)
 print(len(words))
+print(f"your hangman word is {len(random_word)} characters long, start with your first guess")
 print(random_word)
