f44c9092c83bbb1d3316c290eccbca4bfbc899a3
zzachjoness <zzachjoness@gmail.com>
2022-07-18 16:10:46 -0400

    remove unsuable words

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,6 +1,10 @@
-import words
 import random
-words = words.words
+from words import words
+
+# alternative import
+# import words
+# words = words.words
+
 random_word = random.choice(words)
 print(len(words))
 print(f"your hangman word is {len(random_word)} characters long, start with your first guess")
