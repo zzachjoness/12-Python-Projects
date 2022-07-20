63d5e42cc648cda84cca03552aa54671257dc8b5
zzachjoness <zzachjoness@gmail.com>
2022-07-20 17:02:20 -0400

    hangman modify word_letters

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,4 +1,5 @@
 import random
+import string
 from words import words
 
 # alternative import
@@ -6,8 +7,28 @@ from words import words
 # words = words.words
 
 word = random.choice(words)
-print(len(words))
 print(f"your hangman word is {len(word)} characters long, start with your first guess")
 print(word)
-print("trying again")
-guess = input("type something:")
+
+def hangman():
+  word_letters = set(word.upper())
+  alphabet = set(string.ascii_uppercase)
+  used_letters = set()
+  print('word letters: ', word_letters)
+  while len(word_letters) > 0:
+    print('You have used these letters: ',' '.join(used_letters))
+    #List Comprehension
+    word_list = [letter for letter in word_letters & used_letters or '-']
+    print('wordlist: ',' '.join(word_list))
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+      used_letters.add(user_letter)
+      if user_letter in word_letters:
+        print('good guess')
+        word_letters.remove(user_letter)
+    elif user_letter in used_letters:
+        print('you already used that character, try again dope')
+    else:
+      print("invalid character, try again asshole")
+
+hangman()
