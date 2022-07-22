6fac7a1d9d3c49ee789aebef06a60847e3728650
zzachjoness <zzachjoness@gmail.com>
2022-07-22 00:48:50 -0400

    hangman update

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -5,23 +5,31 @@ from words import words
 # alternative import
 # import words
 # words = words.words
-
-word = random.choice(words)
-print(f"your hangman word is {len(word)} characters long, start with your first guess")
-print(word)
+def pick_word():
+  word = random.choice(words)
+  print(f"your hangman word is {len(word)} characters long, start with your first guess")
+  print(word)
+  return word
 
 def hangman():
+
+  word = pick_word()
   word_letters = set(word.upper())
   alphabet = set(string.ascii_uppercase)
   used_letters = set()
-  print('word letters: ', word_letters)
+  user_word = set()
+  
   while len(word_letters) > 0:
-    print('You have used these letters: ',' '.join(used_letters))
-    #List Comprehension
-    word_list = [letter for letter in word_letters & used_letters or '-']
-    print('wordlist: ',' '.join(word_list))
+    print('used_letters: ',' '.join(used_letters))
+    print('word letters:',' '.join(word_letters))
     user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
+    user_word = [for letter in word:
+      if user_letter in word_letters:
+        return letter.upper()
+      else:
+        return '-']
+
+    if user_letter in alphabet and user_letter not in used_letters:
       used_letters.add(user_letter)
       if user_letter in word_letters:
         print('good guess')
